8a32fd16b00918ceac952c129f3a56b3d6a10026
Ethan <ethan@inclushe.com>
2022-02-10 03:11:50 -0500

    Figure out what's crashing Netlify build process

--- encrypt.js
+++
@@ -1,8 +0,0 @@
-var CryptoJS = require("crypto-js");
-
-// Decrypt
-var bytes  = CryptoJS.AES.decrypt('U2FsdGVkX1+DEuTUckU8SWPMMm65nhMLY0Ap80IM/XO43tESbJSXV1eSTqMKN+ZTJJ0DRtHspidsub3qJNNpaQ==');
-var originalText = bytes.toString(CryptoJS.enc.Utf8);
-
-console.log(originalText); // 'my message'
-console.log(originalText.length === 0); // 'my message'
--- script.js
+++ script.js
@@ -1,36 +1,107 @@
-var axios = require('axios');
-var fs = require('fs');
-var base64 = require('base-64');
+console.log('hi!')
+// const { Octokit } = require("@octokit/core");
+// var octokit;
+// var CryptoJS = require("crypto-js");
+// const base64 = require('base-64');
+// const auth_encrypted = 'U2FsdGVkX1+DEuTUckU8SWPMMm65nhMLY0Ap80IM/XO43tESbJSXV1eSTqMKN+ZTJJ0DRtHspidsub3qJNNpaQ==';
+// var authKey = localStorage.getItem("authKey") || '';
+// var isDecrypted = localStorage.getItem("isDecrypted") || false;
+// var attemptForm = document.querySelector("#attempt");
+// var ethanForm = document.querySelector("#ethan");
+// ethanForm.hidden = true;
 
-let token = "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"
-let file = "Welcome to zombo com";
-console.log(file);
-var content = base64.encode(file);
-console.log(content);
-uploadFileApi(token, content)
+// if (isDecrypted) {
+//   octokit = new Octokit({auth: authKey});
+//   attemptForm.hidden = true;
+//   ethanForm.hidden = false;
+// }
 
-function uploadFileApi(token, content) {
+// attemptForm.addEventListener("submit", function(e) {
+//   e.preventDefault();
+//   console.log(attemptForm.password.value)
+//   attempt_decryption(attemptForm.password.value);
+// });
+// ethanForm.addEventListener("submit", function(e) {
+//   e.preventDefault();
+//   console.log(ethanForm.text.value)
+//   create_or_update_post(ethanForm.text.value);
+// });
 
-    var data = JSON.stringify({
-        "message": "txt file",
-        "content": `${content}`
-    });
+// function attempt_decryption(password) {
+//   if (isDecrypted) return;
+//   var bytes  = CryptoJS.AES.decrypt(auth_encrypted, password);
+//   var decryptAttempt = bytes.toString(CryptoJS.enc.Utf8);
+//   if (decryptAttempt.length === 0) {
+//     // Decryption failed
+//     console.log("Decryption failed");
+//     return;
+//   } 
+//   // Decryption succeeded
+//   authKey = decryptAttempt;
+//   isDecrypted = true;
+//   localStorage.setItem("authKey", authKey);
+//   localStorage.setItem("isDecrypted", isDecrypted);
+//   console.log("Decryption succeeded");
+//   console.log(`authKey: ${authKey}`);
+//   octokit = new Octokit({auth: authKey});
+//   attemptForm.hidden = true;
+//   ethanForm.hidden = false;
+//   // octokit.request('GET /rate_limit');
+// }
 
-    var config = {
-        method: 'put',
-        url: 'https://api.github.com/repos/kangae-burner/kangae-data/contents/abc.txt',
-        headers: {
-            'Authorization': `Bearer ${token}`,
-            'Content-Type': 'application/json'
-        },
-        data: data,
-    };
+// function create_or_update_post(content) {
+//   if (!isDecrypted) return;
+//   octokit.request('GET /repos/kangae-burner/kangae-data/contents/', {
+//     owner: 'kangae-burner',
+//     repo: 'kangae-data'
+//   })
+//     .then(data => {
+//       console.log(data.data)
+//       let obj = data.data.find(o => o.name === 'ethan');
+//       return octokit.request('PUT /repos/kangae-burner/kangae-data/contents/ethan', {
+//         owner: 'kangae-burner',
+//         repo: 'kangae-data',
+//         message: 'message',
+//         content: base64.encode(content),
+//         sha: obj.sha
+//       })
+//     })
+//     .then(data => {
+//       console.log(data);
+//     });
+// }
 
-    axios(config)
-        .then(function (response) {
-            console.log(JSON.stringify(response.data));
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-}
+// // Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
+
+// // octokit.request('GET /rate_limit');
+
+// // new Octokit({ auth: `` });
+
+// // octokit.request('GET /repos/kangae-burner/kangae-data/contents/', {
+// //   owner: 'kangae-burner',
+// //   repo: 'kangae-data'
+// // })
+// //   .then(data => {
+// //     console.log(data.data)
+// //     let obj = data.data.find(o => o.name === 'ethan');
+// //     return octokit.request('PUT /repos/kangae-burner/kangae-data/contents/ethan', {
+// //       owner: 'kangae-burner',
+// //       repo: 'kangae-data',
+// //       message: 'message',
+// //       content: base64.encode('Hello Again ' . Math.random()),
+// //       sha: obj.sha
+// //     })
+// //   })
+// //   .then(data => {
+// //     console.log(data);
+// //   });
+
+// // octokit.request('PUT /repos/kangae-burner/kangae-data/contents/ethan', {
+// //   owner: 'kangae-burner',
+// //   repo: 'kangae-data',
+// //   message: 'message',
+// //   content: base64.encode('Hello World'),
+// // })
+// //   .then(data => {
+// //     console.log(data);
+// //   });
