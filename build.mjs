5885e69a9ca1c734a39a601241e1546c5a027be1
Ethan <ethan@inclushe.com>
2022-02-10 11:45:11 -0500

    Add build.mjs and CNAME record

---
+++ build.mjs
@@ -0,0 +1,20 @@
+import * as fs from 'fs';
+import {Parcel} from '@parcel/core';
+
+let bundler = new Parcel({
+  entries: 'index.html',
+  defaultConfig: '@parcel/config-default'
+});
+
+try {
+  let {bundleGraph, buildTime} = await bundler.run();
+  let bundles = bundleGraph.getBundles();
+  console.log(`✨ Built ${bundles.length} bundles in ${buildTime}ms!`);
+  // File destination.txt will be created or overwritten by default.
+  fs.copyFile('CNAME', 'dist/CNAME', (err) => {
+    if (err) throw err;
+    console.log('CNAME was copied to dist/CNAME');
+  });
+} catch (err) {
+  console.log(err.diagnostics);
+}
