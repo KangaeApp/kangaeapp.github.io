d4c6a4cb8b48eaf8bf4c4e98049e8788d9bc0d73
Ethan <ethan@inclushe.com>
2022-02-10 13:00:54 -0500

    Whoops

--- build.mjs
+++ build.mjs
@@ -1,9 +1,7 @@
 import * as fs from 'fs';
 
-try {
-  let {bundleGraph, buildTime} = await bundler.run();
-  let bundles = bundleGraph.getBundles();
-  console.log(`✨ Built ${bundles.length} bundles in ${buildTime}ms!`);
-} catch (err) {
-  console.log(err.diagnostics);
-}
+// File destination.txt will be created or overwritten by default.
+fs.copyFile('CNAME', 'docs/CNAME', (err) => {
+  if (err) throw err;
+  console.log('CNAME was copied to docs/CNAME');
+});
